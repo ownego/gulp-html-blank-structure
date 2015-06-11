cf61b373c470c840bcc1fb16eb9dc432983bf580
JuzSer <sonnh.90@gmail.com>
2015-06-11 17:06:44 +0700

    Structure almost done

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+(function(window, $){
+    var $doc = $(document);
+
+    var config = {
+    }
+
+    // Common functions
+    var oe = {
+    };
+
+    // Make it global
+    window.oe = oe;
+})(window, jQuery);
+
+$(function() {
+
+
+});
